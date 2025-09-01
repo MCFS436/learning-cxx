074be1fe9af32a3b4d2c2dcc079359515d5fad24
MCFS436 <596170708@qq.com>
2025-09-01 23:36:36 +0800

    Update main.cpp

--- exercises/02_function/main.cpp
+++ exercises/02_function/main.cpp
@@ -4,7 +4,7 @@
 // NOTICE: cppreference 中的示例中指出了复杂声明的解读法，建议认真阅读。
 // NOTICE: 补充由内而外读法的机翻解释 <https://learn.microsoft.com/zh-cn/cpp/c-language/interpreting-more-complex-declarators?view=msvc-170>
 
-int add(int a;int b);// TODO: 在这里声明函数
+int add(int a,int b);// TODO: 在这里声明函数
 
 int main(int argc, char **argv) {
     ASSERT(add(123, 456) == 123 + 456, "add(123, 456) should be 123 + 456");
