8a98c862993531884b10394db948d4814fe12191
MCFS436 <596170708@qq.com>
2025-09-01 23:19:11 +0800

    Update main.cpp

--- exercises/13_class/main.cpp
+++ exercises/13_class/main.cpp
@@ -14,13 +14,15 @@ class Fibonacci {
 
 public:
     // TODO: 实现构造器
-    
-    cache[0]=0;
-    cache[1]=1;
-
+    // Fibonacci()
+    auto ini(){
+        cache[0]=0;
+        cache[1]=1;
+        cached=2;
+    }
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (cached=2; cached<=i; ++cached) {
+        for (; cached<=i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
@@ -31,6 +33,7 @@ int main(int argc, char **argv) {
     // 现在类型拥有无参构造器，声明时会直接调用。
     // 这个写法不再是未定义行为了。
     Fibonacci fib;
+    fib.ini();
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
     return 0;
